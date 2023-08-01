f998a43b78b76ab7473fde55d65b242695f5061d
joelohr <emailme@joelohr.com>
2023-08-01 15:13:21 -0500

    Added Shield for permissions

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->plugins([
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
+            ])
             ->colors([
                 'primary' => Color::Amber,
             ])
