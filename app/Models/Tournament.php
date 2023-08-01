f638e1095ca7b25d6c4c432cbaf123b51f578a59
joelohr <emailme@joelohr.com>
2023-08-01 15:26:26 -0500

    Tournament Model and Migration

---
+++ app/Models/Tournament.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Tournament extends Model
+{
+    protected $fillable = [
+        'name',
+        'start_date',
+        'end_date',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
